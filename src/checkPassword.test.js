58b784d3bc8e2b53365f9732221d0742211595e7
Robert <robert.szalapak@gmail.com>
2025-09-28 14:44:05 +0200

    check_password

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,27 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('Password')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('P@ssword1')).toBe(true);
   });
 
-  // write more tests here
+  it(`should accept only letters from the latin alphabet`, () => {
+    expect(checkPassword('P@дддssword1')).toBe(false);
+  });
+
+  it(`should have at least 8 characters`, () => {
+    expect(checkPassword('P@d1')).toBe(false);
+  });
+
+  it(`should have maximum 16 characters`, () => {
+    expect(checkPassword('P@assword123445678900000')).toBe(false);
+  });
+
+  it(`should have
+     at least 1 digit, 1 special character, 1 uppercase letter`, () => {
+    expect(checkPassword('password')).toBe(false);
+  });
 });
